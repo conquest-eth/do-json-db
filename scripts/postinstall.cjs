1ce01fe1d9d762e7140f5c718a4f0afe68be5e7e
wighawag <wighawag@gmail.com>
2022-07-04 08:43:28 +0100

    cleanup

--- scripts/postinstall.cjs
+++ scripts/postinstall.cjs
@@ -4,50 +4,54 @@ const path = require('path');
 const root = path.parse(process.cwd()).root;
 
 function getPackageJSONPath(startPath) {
-	startPath = startPath || process.cwd();
+  startPath = startPath || process.cwd();
 
-	let searchPath = startPath
-	let fileFound = false;
+  let searchPath = startPath;
+  let fileFound = false;
   let filepath;
-	while (!fileFound) {
+  while (!fileFound) {
     filepath = path.join(searchPath, 'package.json');
     fileFound = fs.existsSync(filepath);
     if (!fileFound) {
       const newPath = path.join(searchPath, '..');
-      if (newPath === searchPath || newPath === root || newPath === '.' || newPath === '..') {
+      if (
+        newPath === searchPath ||
+        newPath === root ||
+        newPath === '.' ||
+        newPath === '..'
+      ) {
         break;
       }
       searchPath = newPath;
     }
-	}
+  }
 
-	if (fileFound) {
-		return path.normalize(filepath);
-	}
+  if (fileFound) {
+    return path.normalize(filepath);
+  }
 }
 
-
 // from https://stackoverflow.com/a/26038979
-function copyFileSync( source, target ) {
+function copyFileSync(source, target) {
   var targetFile = target;
-  if ( fs.existsSync( target ) ) {
-    if ( fs.lstatSync( target ).isDirectory() ) {
-      targetFile = path.join( target, path.basename( source ) );
-      }
+  if (fs.existsSync(target)) {
+    if (fs.lstatSync(target).isDirectory()) {
+      targetFile = path.join(target, path.basename(source));
+    }
   }
   fs.writeFileSync(targetFile, fs.readFileSync(source));
 }
 
-function copyFolderRecursiveSync( source, target ) {
+function copyFolderRecursiveSync(source, target) {
   var files = [];
-  if (!fs.existsSync(target) ) {
-    fs.mkdirSync(target, {recursive: true});
+  if (!fs.existsSync(target)) {
+    fs.mkdirSync(target, { recursive: true });
   }
   if (fs.lstatSync(source).isDirectory()) {
     files = fs.readdirSync(source);
     files.forEach(function (file) {
       var curSource = path.join(source, file);
-      if (fs.lstatSync(curSource ).isDirectory()) {
+      if (fs.lstatSync(curSource).isDirectory()) {
         copyFolderRecursiveSync(curSource, path.join(target, file));
       } else {
         copyFileSync(curSource, target);
@@ -56,12 +60,11 @@ function copyFolderRecursiveSync( source, target ) {
   }
 }
 
-
 function copy(destination) {
   if (fs.existsSync(destination)) {
-    fs.rmSync(destination, {recursive: true});
+    fs.rmSync(destination, { recursive: true });
   }
-  fs.mkdirSync(destination, {recursive: true});
+  fs.mkdirSync(destination, { recursive: true });
 
   // TODO automate from package.json "files"
   fs.copyFileSync(
@@ -74,20 +77,25 @@ function copy(destination) {
   );
 }
 
-
 let filepath;
-if(process.env.INIT_CWD) {
+if (process.env.INIT_CWD) {
   filepath = getPackageJSONPath(process.env.INIT_CWD);
 }
 
 if (!filepath) {
-  console.error(`could not find package.json of parent package${process.env.INIT_CWD ? '' : ' (no INIT_CWD)'}`)
+  console.error(
+    `could not find package.json of parent package${
+      process.env.INIT_CWD ? '' : ' (no INIT_CWD)'
+    }`,
+  );
 } else {
   const content = fs.readFileSync(filepath, 'utf8');
   const json = JSON.parse(content);
   if (json.typescriptLibraries) {
     if (typeof json.typescriptLibraries !== 'string') {
-      console.error(`typescriptLibraries is expectedted to be a string field in package.json`)
+      console.error(
+        `typescriptLibraries is expectedted to be a string field in package.json`,
+      );
       process.exit(1);
     }
     const currentPackage = process.env['npm_config_dir'];
@@ -101,12 +109,16 @@ if (!filepath) {
       process.exit(0);
     }
     if (!fs.existsSync(p)) {
-      console.error(`current package package.json not found`)
+      console.error(`current package package.json not found`);
       process.exit(1);
     }
 
     const packageName = process.env['npm_package_name'];
-    const destination = path.join(path.dirname(filepath), json.typescriptLibraries, packageName);
+    const destination = path.join(
+      path.dirname(filepath),
+      json.typescriptLibraries,
+      packageName,
+    );
     // const source = process.env['npm_config_dir'];
     // console.log({source, destination});
     copy(destination);
@@ -114,22 +126,3 @@ if (!filepath) {
     // console.error(`typescriptLibraries not set`)
   }
 }
-
-/*
-fs.copyFileSync(
-    path.join(__dirname, '../tsconfig.json'),
-    path.join(destination, 'tsconfig.json'),
-  );
-  fs.copyFileSync(
-    path.join(__dirname, '../env.d.ts'),
-    path.join(destination, 'env.d.ts'),
-  );
-  copyFolderRecursiveSync(
-    path.join(__dirname, '../lib/'),
-    path.join(destination, 'lib'),
-  );
-  copyFolderRecursiveSync(
-    path.join(__dirname, '../src/'),
-    path.join(destination, 'src'),
-  );
-  */
